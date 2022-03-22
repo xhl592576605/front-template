6ad723db2358113a6a6ebc0a9f669bc999c6f82f
xuhuale <592576605@qq.com>
2022-03-22 21:34:11 +0800

    fix: 测试提交

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import '@/assets/style/index.less';
+import '@/assets/style/index.less'
 import '@/assets/style/index.scss'
 ;(() => {
-  console.log('hi, webpack.');
-})();
+  console.log('hi, webpack.')
+})()
 
-console.log(111);
-
-
+console.log(111)
+console.log(111)
 console.log(111)
