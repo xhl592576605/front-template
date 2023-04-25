4c2cd5a24861e794e2439f074731082bb3d0078c
xuhuale <xuhuale@star-net.cn>
2023-04-25 15:10:10 +0800

    add new version 0.0.6

--- script/build/index.js
+++ script/build/index.js
@@ -16,6 +16,20 @@ const buildWin = () => {
     )
   })
 }
+const buildMac = () => {
+  return new Promise((resolve, reject) => {
+    shell.exec(
+      `electron-builder -w=nsis --x64 --arm64 --config ./script/build/config/mac.js `,
+      (code, stdout) => {
+        if (code !== 0) {
+          reject('打包win失败')
+          return
+        }
+        resolve()
+      }
+    )
+  })
+}
 module.exports = () => {
   return new Promise((resolve, reject) => {
     log.info('开始打包electron')
@@ -23,7 +37,7 @@ module.exports = () => {
     const platform = process.platform
     if (platform === 'darwin') {
       log.info('处于mac环境,同时打包mac和win')
-      buildWin()
+      buildWin().then(buildMac).then(resolve).catch(reject)
       return
     } else if (platform === 'win32') {
       log.info('处于win环境,只打包win')
