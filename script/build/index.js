7ba8bb0f7151ac44421895d51eedd0122bb4c3b3
xuhuale <xuhuale@star-net.cn>
2023-04-25 14:22:45 +0800

    add new version 0.0.4

--- script/build/index.js
+++ script/build/index.js
@@ -5,17 +5,20 @@ const builderConfig = require('./config/electron-builder')
 module.exports = () => {
   return new Promise((resolve, reject) => {
     log.info('开始打包electron')
+    // 使用electron-builder打包
     builder
       .build({
         targets: builder.Platform.WINDOWS.createTarget(),
-        win: ['nsis'],
         x64: true,
         config: builderConfig
       })
-      .then(resolve)
-      .catch((err) => reject(err))
-      .finally(() => {
-        log.info('结束打包electron')
+      .then(() => {
+        log.success('electron打包成功')
+        resolve()
+      })
+      .catch((err) => {
+        log.error('electron打包失败: ' + err.message)
+        reject(err)
       })
   })
 }
